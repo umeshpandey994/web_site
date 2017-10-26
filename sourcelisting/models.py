4e8746af988e60b943a0b317922ee9e53ec808f8
uvirus <umesh.pandey994@gmail.com>
2017-10-26 06:28:23 +0530

    added html pop up

--- sourcelisting/models.py
+++ sourcelisting/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 from subscribers.models import Company
+import urllib
 
 
 class Source(models.Model):
@@ -21,8 +22,24 @@ class Source(models.Model):
     class Meta:
         unique_together = (("url", "created_by"),)
 
+    def aid_link(self):
+        story_parms = {
+            'source': self.id
+        }
+        encoded_params = urllib.urlencode(story_parms)
+        popup = """<b><a href='#' onclick="return popitup('/admin/stories/story/add/?{}', '{}')">{}</a></b>""".format(encoded_params, self.url, self.name)
+        return popup
 
 
+    aid_link.allow_tags = True
+    #aid_link.admin_order_field = 'name'
 
-
-
+    def test_popup(self):
+        return """
+            <a href="{}" 
+                target="popup" 
+                onclick="window.open('{}','popup','width=600,height=600'); return false;  window.open('http://kanishkkunal.in','popup','width=600,height=600'); return false;">
+                Open Link in Popup
+            </a>
+        """.format(self.url, self.url)
+    test_popup.allow_tags = True
